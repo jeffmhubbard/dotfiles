4900d60ca141fe13c969373ddf19f63155ea9bb5
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2018-04-26 05:14:31 -0500

    Changed font and size

--- src/.config/qutebrowser/config.py
+++ src/.config/qutebrowser/config.py
@@ -757,39 +757,39 @@ c.colors.webpage.bg = xres['*color15']
 
 ## Font used in the completion categories.
 ## Type: Font
-c.fonts.completion.category = 'bold 10pt monospace'
+c.fonts.completion.category = 'bold 9pt monospace'
 
 ## Font used in the completion widget.
 ## Type: Font
-c.fonts.completion.entry = '10pt monospace'
+c.fonts.completion.entry = '9pt monospace'
 
 ## Font used for the debugging console.
 ## Type: QtFont
-c.fonts.debug_console = '10pt monospace'
+c.fonts.debug_console = '9pt monospace'
 
 ## Font used for the downloadbar.
 ## Type: Font
-c.fonts.downloads = '10pt monospace'
+c.fonts.downloads = '9pt monospace'
 
 ## Font used for the hints.
 ## Type: Font
-c.fonts.hints = 'bold 10pt monospace'
+c.fonts.hints = 'bold 9pt monospace'
 
 ## Font used in the keyhint widget.
 ## Type: Font
-c.fonts.keyhint = '10pt monospace'
+c.fonts.keyhint = '9pt monospace'
 
 ## Font used for error messages.
 ## Type: Font
-c.fonts.messages.error = 'bold 10pt monospace'
+c.fonts.messages.error = 'bold 9pt monospace'
 
 ## Font used for info messages.
 ## Type: Font
-c.fonts.messages.info = 'bold 10pt monospace'
+c.fonts.messages.info = 'bold 9pt monospace'
 
 ## Font used for warning messages.
 ## Type: Font
-c.fonts.messages.warning = 'bold 10pt monospace'
+c.fonts.messages.warning = 'bold 9pt monospace'
 
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
@@ -798,15 +798,15 @@ c.fonts.monospace = 'Source Code Pro'
 
 ## Font used for prompts.
 ## Type: Font
-c.fonts.prompts = '10pt monospace'
+c.fonts.prompts = '9pt monospace'
 
 ## Font used in the statusbar.
 ## Type: Font
-c.fonts.statusbar = '10pt monospace'
+c.fonts.statusbar = '9pt monospace'
 
 ## Font used in the tab bar.
 ## Type: QtFont
-c.fonts.tabs = '10pt monospace'
+c.fonts.tabs = '9pt monospace'
 
 ## Font family for cursive fonts.
 ## Type: FontFamily
