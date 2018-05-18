9897dc8d02c9e092aa118ef3136e217ab8faed5f
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2018-05-18 16:29:41 -0500

    Reorganize polybar conf TBC

--- src/.config/polybar/launch.sh
+++ src/.config/polybar/launch.sh
@@ -7,9 +7,22 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-if [[ $(hostname) == dragon ]]; then
-    polybar left &
-    polybar right &
-else
-    polybar bottom &
-fi
+#if [[ $(hostname) == dragon ]]; then
+#    polybar left &
+#    polybar right &
+#else
+#    polybar bottom &
+#fi
+
+case $(hostname) in
+    dragon)       
+        polybar dragon-left &
+        polybar dragon-right &
+        ;;
+    gargoyle)
+        polybar gargoyle-bottom &
+        ;;            
+    *)              
+        polybar base-bottom &
+esac 
+
