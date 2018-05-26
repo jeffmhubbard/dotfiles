44e2316dab5e5b37cbdf544340de8a322fa94531
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2018-05-25 21:53:36 -0500

    Moar vim

--- extras/vim_setup.sh
+++ extras/vim_setup.sh
@@ -4,4 +4,5 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/ap/vim-buftabline.git ~/.vim/bundle/vim-buftabline
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
