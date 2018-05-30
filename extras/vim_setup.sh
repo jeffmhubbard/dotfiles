3e71c503bbef6a5b3e8cd144b2fa0163a07d8124
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2018-05-30 07:42:56 -0500

    Update vim_setup.sh

--- extras/vim_setup.sh
+++ extras/vim_setup.sh
@@ -2,7 +2,13 @@
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/itchyny/lightline.vim.git ~/.vim/bundle/lightline.vim
+git clone https://github.com/ryanoasis/vim-devicons.git ~/.vim/bundle/vim-devicons
 git clone https://github.com/ap/vim-buftabline.git ~/.vim/bundle/vim-buftabline
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone https://github.com/mhinz/vim-signify.git ~/.vim/bundle/vim-signify
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+git clone https://github.com/vifm/vifm.vim.git ~/.vim/bundle/vifm.vim
