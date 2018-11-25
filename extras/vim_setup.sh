97a6608b8fda52b2f75fd8f661c0f3467a630ac6
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2018-11-25 04:05:00 -0600

    Add fugitive to statusline, add tagbar

--- extras/vim_setup.sh
+++ extras/vim_setup.sh
@@ -14,6 +14,7 @@ git clone https://github.com/ryanoasis/vim-devicons.git ~/.vim/bundle/vim-devico
 git clone https://github.com/ap/vim-buftabline.git ~/.vim/bundle/vim-buftabline
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/mhinz/vim-signify.git ~/.vim/bundle/vim-signify
 git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
