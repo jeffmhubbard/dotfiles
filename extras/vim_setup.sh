b9a0dd790a2b694ee9d8ac39e4fbb0d82f3ad73a
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2019-01-30 19:24:41 -0600

    Beef up vim setup

--- extras/vim_setup.sh
+++ extras/vim_setup.sh
@@ -2,30 +2,42 @@
 
 # extras/vim_setup.sh
 
+
+PATHOGEN="https://tpo.pe/pathogen.vim"
+BUNDLE_DIR="$HOME/.vim/bundle"
+AUTOLOAD_DIR="$HOME/.vim/autoload"
+
+declare -a PLUGINS
+PLUGINS=(
+    https://github.com/w0rp/ale.git
+    https://github.com/jeffmhubbard/antsy.vim.git
+    https://github.com/junegunn/fzf.vim.git
+    https://github.com/haya14busa/incsearch.vim.git
+    https://github.com/scrooloose/nerdtree.git
+    https://github.com/Xuyuanp/nerdtree-git-plugin.git
+    https://github.com/itchyny/lightline.vim.git
+    https://github.com/maximbaz/lightline-ale.git
+    https://github.com/majutsushi/tagbar.git
+    https://github.com/ap/vim-buftabline.git
+    https://github.com/ryanoasis/vim-devicons.git
+    https://github.com/tpope/vim-fugitive.git
+    https://github.com/airblade/vim-gitgutter.git
+    https://github.com/farmergreg/vim-lastplace.git
+    https://github.com/mhinz/vim-signify.git
+    https://github.com/tpope/vim-surround.git
+)
+
 # create directories
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ${AUTOLOAD_DIR} ${BUNDLE_DIR}
 
 # install pathogen
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ${AUTOLOAD_DIR}/$(basename "$PATHOGEN") https://tpo.pe/pathogen.vim
 
 # install plugins
-git clone https://github.com/ap/vim-buftabline.git ~/.vim/bundle/vim-buftabline
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
-git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
-git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
-git clone https://github.com/itchyny/lightline.vim.git ~/.vim/bundle/lightline.vim
-git clone https://github.com/maximbaz/lightline-ale.git ~/.vim/bundle/lightline-ale
-git clone https://github.com/w0rp/ale.git ~/.vim/bundle/ale
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
-git clone https://github.com/mhinz/vim-signify.git ~/.vim/bundle/vim-signify
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
-git clone https://github.com/farmergreg/vim-lastplace.git ~/.vim/bundle/vim-lastplace
-git clone https://github.com/ryanoasis/vim-devicons.git ~/.vim/bundle/vim-devicons
-
-# install colorscheme
-git clone https://github.com/jeffmhubbard/antsy.vim.git ~/.vim/bundle/antsy.vim
-
+for url in "${PLUGINS[@]}"
+do
+    repo=$(basename "$url" ".${url##*.}")
+    git clone ${url} ${BUNDLE_DIR}/${repo}
+done
 
 # vim: set ft=sh:
