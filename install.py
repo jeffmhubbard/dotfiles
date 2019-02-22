e58371ab340f88d3b370d4081a1386171742f590
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2019-02-22 16:59:36 -0600

    Oops broke install.py

--- install.py
+++ install.py
@@ -3,5 +3,6 @@
 from distutils.dir_util import copy_tree
 from os import path
 
-if input("Install? (yes/no): ").lower().strip()[:1] == "yes":
+if input("Install? (yes/no): ").lower().strip() == "yes":
+    print("Okay...")
     copy_tree('src/', path.expanduser('~'))
