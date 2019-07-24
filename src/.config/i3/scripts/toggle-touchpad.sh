754128b8d233d17a1b72e2e8f0b413c6e26c0947
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2019-07-24 08:42:02 -0500

    i3 tweaks

--- src/.config/i3/scripts/toggle-touchpad.sh
+++ src/.config/i3/scripts/toggle-touchpad.sh
@@ -3,18 +3,22 @@
 ICON=/usr/share/icons/oomox-antsy/devices/scalable/input-mouse.svg
 
 declare -i ID
-ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+if [[ -n $TOUCHPAD ]]; then
+    ID="$TOUCHPAD"
+else
+    ID=$(xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}')
+fi
 
 declare -i STATE
-STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
+STATE=$(xinput list-props "$ID" | grep 'Device Enabled' | awk '{print $4}')
 
-if [ $STATE -eq 1 ]
+if [ "$STATE" -eq 1 ]
 then
-    xinput disable $ID
-    # echo "Touchpad disabled."
-    dunstify -u normal -t 1000 Touchpad 'Disabled' -i $ICON
+    xinput disable "$ID"
+    CURSTATE="Touchpad Disabled"
 else
-    xinput enable $ID
-    # echo "Touchpad enabled."
-    dunstify -u normal -t 2500 Touchpad 'Enabled' -i $ICON
+    xinput enable "$ID"
+    CURSTATE="Touchpad Enabled"
 fi
+
+dunstify -u normal -t 2500 "$CURSTATE" -i "$ICON"
