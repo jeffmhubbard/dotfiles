b20232b172db6a37239d9d562caa00d3fb8e5406
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2019-11-21 19:30:29 -0600

    Add script to setup and start xob

---
+++ src/.config/xob/load-bars.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# xob/load-bars.sh
+
+BARS=(volume backlight)         # bars must be specified in styles.cfg
+CACHE_DIR="$HOME/.cache/xob"    # directory where pipes will be created
+
+# create cache dir
+mkdir "$CACHE_DIR" 2&> /dev/null
+
+for bar in "${BARS[@]}"; do
+  # make pipe
+  pipe="$CACHE_DIR/$bar.fifo"
+  mkfifo "$pipe" 2&> /dev/null
+  # watch pipe
+  (tail -f "$pipe" | xob -s "$bar")&
+done
+
+exit 0
+
+# vim: ft=sh ts=2 sw=0 et:
