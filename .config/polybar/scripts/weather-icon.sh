df473c2eff3613c679aaee8d3f8807eb1bfc5d21
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2020-02-26 08:43:53 -0600

    Fix glyphs being clipped

--- .config/polybar/scripts/weather-icon.sh
+++ .config/polybar/scripts/weather-icon.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # weather-icon.sh
-#
 
 CITY="4671654"
 UNITS="imperial"
@@ -14,25 +13,25 @@ KEY="5a8ee2a1219f6211e45343a8f8276118"
 # use nerd fonts
 get_icon() {
   case $1 in
-    01d) icon="";;
-    01n) icon="";;
+    01d) icon=" ";;
+    01n) icon=" ";;
     02d) icon="杖";;
-    02n) icon="";;
-    03*) icon="";;
-    04*) icon="";;
-    09d) icon="";;
-    09n) icon="";;
-    10d) icon="";;
-    10n) icon="";;
-    11d) icon="";;
-    11n) icon="";;
-    13d) icon="";;
-    13n) icon="";;
-    50d) icon="";;
-    50n) icon="";;
-    *) icon="";
+    02n) icon=" ";;
+    03*) icon=" ";;
+    04*) icon=" ";;
+    09d) icon=" ";;
+    09n) icon=" ";;
+    10d) icon=" ";;
+    10n) icon=" ";;
+    11d) icon=" ";;
+    11n) icon=" ";;
+    13d) icon=" ";;
+    13n) icon=" ";;
+    50d) icon=" ";;
+    50n) icon=" ";;
+    *) icon=" ";
   esac
-  echo $icon
+  echo "$icon"
 }
 
 # get weather results
@@ -56,7 +55,7 @@ fi
 if [ -n "$weather" ]; then
   weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
   weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
-  echo "%{F${COLOR}}%{T2}$(get_icon "$weather_icon")%{T-}%{F-}" "$weather_temp%{T2}$SYMBOL%{T-}"
+  echo "%{F${COLOR}}%{T2}$(get_icon "$weather_icon")%{T-}%{F-} $weather_temp%{T2}$SYMBOL%{T-}"
 fi
 
 # vim: ft=sh ts=2 sw=0 et:
