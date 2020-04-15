2e1dd49a5e6f18d34cd3ebbec41ada25940bc15f
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2020-04-15 18:46:33 -0500

    Update tmux setup script

--- .bin/tmux_setup.sh
+++ .bin/tmux_setup.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-# extras/tmux_setup.sh
 # install tpm
 
+TPM_URL="https://github.com/tmux-plugins/tpm.git"
+PLUGIN_DIR="$HOME/.config/tmux/plugins"
+
+# create directories
+echo "Creating directories..."
+if ! mkdir -p "${PLUGIN_DIR}"; then
+  echo "Failed to create $PLUGIN_DIR!"; exit 1
+fi
+
 echo "Installing TPM..."
-git clone https://github.com/tmux-plugins/tpm.git "$HOME"/.config/tmux/plugins/tpm
+if ! git clone "${TPM_URL}" "${PLUGIN_DIR}/tpm"; then
+  echo "Failed to install TPM!"; exit 1
+fi
 
 echo "Complete"
 exit 0
