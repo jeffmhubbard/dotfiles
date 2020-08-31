13401e444fbf92cbebb4476b3a5636f28a6273e5
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2020-08-30 20:31:10 -0500

    Move i3 scripts to ~/.bin

--- .bin/window-hints.sh
+++ .bin/window-hints.sh
@@ -6,9 +6,9 @@ wmfocus \
     --chars asdf \
     --textcolor '#e9ebeb' \
     --textcoloralt '#14f0f0' \
-    --bgcolor 'rgba(0, 0, 0, 0.7)' \
+    --bgcolor 'rgba(0, 0, 0, 0.9)' \
     --font 'Hack Nerd Font':64 \
     --margin 0.5 \
     --halign left \
     --valign top \
-    --offset 2,2
+    --offset 1,1
--- .config/i3/scripts/start-xob.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-XOB_DIR="$HOME/.config/xob"
-
-for bar in "$XOB_DIR"/*.sh; do
-    bash "$bar"
-done
-
-exit 0
