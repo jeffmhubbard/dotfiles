5ee0abf66186cf0a703bfa5d0671ed7b064ec855
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2020-09-06 19:57:02 -0500

    Moar xob

--- .config/xob/volume.sh
+++ .config/xob/volume.sh
@@ -1,33 +1,18 @@
 #!/usr/bin/env bash
 
-# script should be named after styles defined in styles.cfg
-# $ cp bar.example volume.sh
+cache="$HOME/.cache/xob"    # this is the only "setting"
+style=$(basename "${0%.*}")
+pipe="$cache/$style.fifo"
+pid="$cache/$style.pid"
 
-NAME=$(basename "${0%.*}")
+[[ -d "$cache" ]] || mkdir -p "$cache" &>/dev/null
+[[ -p "$pipe" ]] || mkfifo "$pipe" &>/dev/null
+[[ -f "$pid" ]] || touch "$pid" &>/dev/null
 
-CACHE_DIR="$HOME/.cache/xob"
-OSD_PIPE="$CACHE_DIR/$NAME.fifo"
-OSD_PID="$CACHE_DIR/$NAME.pid"
+last=$(head -n 1 "$pid")
+for id in $(pgrep -f "$style"); do
+    [[ "$id" -eq "$last" ]] && kill -9 "$last" &>/dev/null
+done; unset id
 
-# check cache directory
-[[ -d "$CACHE_DIR" ]] || \
-    mkdir -p "$CACHE_DIR" &>/dev/null
-
-# check named pipe
-[[ -p "$OSD_PIPE" ]] || \
-    mkfifo "$OSD_PIPE" &>/dev/null
-
-# check pid file
-[[ -f "$OSD_PID" ]] || \
-    touch "$OSD_PID" &>/dev/null
-
-# kill running instance
-OLD_PID=$(head -n 1 "$OSD_PID")
-kill -9 "$OLD_PID" &>/dev/null
-
-# watch pipe
-tail -f "$OSD_PIPE" | xob -s "$NAME" &
-
-# write pid
-jobs -p > "$OSD_PID"
-
+tail -f "$pipe" | xob -s "$style" &
+jobs -p > "$pid"
